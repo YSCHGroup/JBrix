a68adef10f5067c9fd446c56e08e21e866cc1041
yschgroup <32958487+YSCHGroup@users.noreply.github.com>
2018-10-30 20:08:46 +0100

    Update attributes/oninstantchange.js

--- attributes/oninstantchange.js
+++ attributes/oninstantchange.js
@@ -1,9 +1,9 @@
-
 function init_oninstantchange() {
     let elms = document.querySelectorAll('[oninstantchange]');
     for (var i = 0; i < elms.length; i++) {
         elms[i].addEventListener("keydown", function(el) {
             setTimeout(() => {
+                console.log(elms[i], elms[i].getAttribute("oninstantchange"));
                 eval(elms[i].getAttribute("oninstantchange"));
             }, 1);
         });
